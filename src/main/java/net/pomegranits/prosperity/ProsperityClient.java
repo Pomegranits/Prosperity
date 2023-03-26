647552b8c00683a912cfe0c3f1030f815ad520cf
pomegranits <Pomegranitsyt@gmail.com>
2023-03-26 14:21:15 +0100

    Basic Setup

---
+++ src/main/java/net/pomegranits/prosperity/ProsperityClient.java
@@ -0,0 +1,10 @@
+package net.pomegranits.prosperity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProsperityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pomegranits/prosperity/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.pomegranits.prosperity.mixin;
+
+import net.pomegranits.prosperity.Prosperity;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Prosperity.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
